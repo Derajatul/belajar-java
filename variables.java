6e7150e35a41b09031ef153a11a11b070626f90f
Moh Derajatul Ula <101638355+Derajatul@users.noreply.github.com>
2023-03-28 09:24:31 +0700

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        // String variable
+        String name = "Nama saya dera";
+        System.out.println(name);
+        
+        // Int variable
+        int myAge = 15;
+        myAge = 18;
+        System.out.println("Umur saya " + myAge + " tahun");
+        
+        // float variable
+        float myWeight = 45.5f;
+        System.out.println(myWeight);
+        
+        // char varirable
+        char myLetter = 'D';
+        System.out.println(myLetter);
+        
+        // boolean variable
+        boolean isStudy = true;
+        System.out.println(isStudy);
+        
+        // final variable
+        final int myHeight = 170;
+        System.out.println(myHeight);
+        
+        // Declare many variable
+        int x = 5, y=10,z=20;
+        System.out.println(x+y+z);
+        
+    }
+}
