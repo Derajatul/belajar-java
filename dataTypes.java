c493359a4ee021ab6ed3ed812abecb2496180919
Moh Derajatul Ula <101638355+Derajatul@users.noreply.github.com>
2023-03-28 09:49:29 +0700

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        
+        String name = "Nama saya dera"; // String
+        
+        byte myNum = 100; // Integer (-127 to 127)
+        short myNums = 5000; // Integer (-36768 to 36768)
+        int myAge = 150000; // Integer (-2147483648 to 2147483647)
+        long myAgg = 10000000; // Integer (-9223372036854775808 to 9223372036854775807)
+        
+        float myWeight = 45.5f; // Floating point number
+        double myWeights = 400.2d; // Floating point number
+        
+        char myLetter = 'D'; // Single character
+        
+        boolean isStudy = true; // Boolean
+    }
+}
